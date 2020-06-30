80500ac94daa2b500a854f95baadc98f6b7bdeb8
Laksh Jadhwani <47735648+lakshjadhwanilj@users.noreply.github.com>
2020-06-30 15:23:49 +0530

    Google oauth

--- config/passport.js
+++ config/passport.js
@@ -6,7 +6,8 @@ module.exports = function (passport) {
     passport.use(new GoogleStrategy({
             clientID: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-            callbackURL: '/auth/google/callback'
+            callbackURL: '/auth/google/callback',
+            callbackURL: 'https://gentle-cove-45415.herokuapp.com/auth/google/callback'
         },
         async (accessToken, refreshToken, profile, done) => {
             const newUser = {
